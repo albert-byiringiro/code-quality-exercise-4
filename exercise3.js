0896fa0e7aae1d9e22f73a677510dd9da1992422
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-23 19:48:05 +0200

    Add exercise 4

--- exercise3.js
+++ exercise3.js
@@ -1,22 +1,4 @@
 function reverseVowels(str) {
-    const vowels = 'aeiouAEIOU';
-    const chars = str.split('');
-    let left = 0;
-    let right = str.length - 1;
-
-    while (left < right) {
-        while (left < right && !vowels.includes(chars[left])) {
-            left++;
-        }
-        while (left < right && !vowels.includes(chars[right])) {
-            right--;
-        }
-        if (left < right) {
-            [chars[left], chars[right]] = [chars[right], chars[left]];
-            left++;
-            right--;
-        }
-    }
-
-    return chars.join('');
-}
+    const vowels = str.match(/[aeiou]/gi) || [];
+    return str.replace(/[aeiou]/gi, () => vowels.pop());
+  }
