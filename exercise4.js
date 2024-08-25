881aee3f7b519479cd42952578656b7626e0b85b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-25 12:58:07 +0200

    Update exercise 4

--- exercise4.js
+++ exercise4.js
@@ -1,17 +1,21 @@
-function fibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-    
-    let sequence = [0, 1];
-    
-    while (sequence[sequence.length - 1] < n) {
-      let nextNum = sequence[sequence.length - 1] + sequence[sequence.length - 2];
-      if (nextNum <= n) {
-        sequence.push(nextNum);
-      } else {
-        break;
-      }
-    }
-    
-    return sequence;
+function generatefibonacciSequence(num) {
+  if (typeof num !== 'number' || num < 0) {
+    throw new Error('Input must be a non-negative integer.');
   }
+
+  if (num === 0) return [0];
+
+  const sequence = [0, 1];
+
+  while (true) {
+    const nextNum = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+
+    if (nextNum > num) break;
+
+    sequence.push(nextNum);
+  }
+
+  return sequence;
+}
+
+console.log(generatefibonacciSequence(17))
