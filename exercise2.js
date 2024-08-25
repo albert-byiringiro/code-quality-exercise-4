ba490addc0887c4b6b54167aa252554d7775211b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-25 11:15:53 +0200

    Update exercise 2

--- exercise2.js
+++ exercise2.js
@@ -1,23 +1,17 @@
-function solution(roman) {
-    const values = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000
-    };
-    
-    let result = 0;
-    
-    for (let i = 0; i < roman.length; i++) {
-      if (i > 0 && values[roman[i]] > values[roman[i-1]]) {
-        result += values[roman[i]] - 2 * values[roman[i-1]];
-      } else {
-        result += values[roman[i]];
-      }
-    }
-    
-    return result;
-  }
+function decodeRomanNumerals(roman) {
+  const values = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  };
+
+  return roman.split("").reduce((result, char, i, arr) => {
+    const currentValue = values[char];
+    const nextValue = values[arr[i + 1]] || 0;
+    return result + (currentValue < nextValue ? -currentValue : currentValue);
+  }, 0)
+}
