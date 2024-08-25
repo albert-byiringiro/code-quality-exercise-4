0120787857dc1109e3fe70ac9465ccc9fd40a96b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-25 12:26:35 +0200

    Update exercise 3

--- exercise3.js
+++ exercise3.js
@@ -1,4 +1,33 @@
 const reverseVowels = (str) => {
-    const vowels = str.match(/[aeiou]/gi) || [];
-    return str.replace(/[aeiou]/gi, () => vowels.pop());
+    if (typeof str !== 'string') {
+        throw new Error("This must be a string");
+    }
+
+    const chars = str.split("");
+
+    const vowels = new Set(['a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I']);
+
+    let left = 0;
+    let right = chars.length - 1;
+
+    while (left < right) {
+        while (left < right && !vowels.has(chars[left])) {
+            left++;
+        }
+
+        while (left < right && !vowels.has(chars[right])) {
+            right--;
+        }
+
+        [chars[left], chars[chars[right]]] = [chars[right], chars[chars[left]]]
+
+        left++;
+        right--;
+    }
+
+    return chars.join("");
 }
+
+console.log('====================================');
+console.log(reverseVowels("peorpleuxioty"));
+console.log('====================================');
